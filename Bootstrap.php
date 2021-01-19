10890f4937d5933229528a9ab736bfba70d11794
Azam2926 <azam.inoyatov.1998@gmail.com>
2021-01-19 10:01:01 +0500

    add datecontrol Module to the app

--- Bootstrap.php
+++ Bootstrap.php
@@ -3,6 +3,7 @@
 namespace afzalroq\cms;
 
 use yii\base\BootstrapInterface;
+use kartik\datecontrol\Module;
 
 
 class Bootstrap implements BootstrapInterface
@@ -26,5 +27,57 @@ class Bootstrap implements BootstrapInterface
                 'forceTranslation' => true,
             ];
         }
+        $app->bootstrap[] = 'cms';
+        $app->setModules([
+            'datecontrol' => [
+                'class' => 'kartik\datecontrol\Module',
+
+                // format settings for displaying each date attribute (ICU format example)
+                'displaySettings' => [
+                    Module::FORMAT_DATE => 'dd-MM-yyyy',
+                    Module::FORMAT_TIME => 'HH:i:s',
+                    Module::FORMAT_DATETIME => 'dd-MM-yyyy HH:i:s',
+                ],
+
+                // format settings for saving each date attribute (PHP format example)
+                'saveSettings' => [
+                    Module::FORMAT_DATE => 'php:U', // saves as unix timestamp
+                    Module::FORMAT_TIME => 'php:H:i:s',
+                    Module::FORMAT_DATETIME => 'php:U',
+                ],
+
+                // set your display timezone
+                'displayTimezone' => 'Asia/Tashkent',
+
+                // set your timezone for date saved to db
+                'saveTimezone' => 'UTC',
+
+                // automatically use kartik\widgets for each of the above formats
+                'autoWidget' => true,
+
+                // use ajax conversion for processing dates from display format to save format.
+                'ajaxConversion' => true,
+
+                // default settings for each widget from kartik\widgets used when autoWidget is true
+                'autoWidgetSettings' => [
+                    Module::FORMAT_DATE => ['type' => 2, 'pluginOptions' => ['autoclose' => true]], // example
+                    Module::FORMAT_DATETIME => [], // setup if needed
+                    Module::FORMAT_TIME => [], // setup if needed
+                ],
+
+                // custom widget settings that will be used to render the date input instead of kartik\widgets,
+                // this will be used when autoWidget is set to false at module or widget level.
+                'widgetSettings' => [
+                    Module::FORMAT_DATE => [
+                        'class' => 'yii\jui\DatePicker', // example
+                        'options' => [
+                            'dateFormat' => 'php:d-M-Y',
+                            'options' => ['class' => 'form-control'],
+                        ]
+                    ]
+                ]
+                // other settings
+            ]
+        ]);
     }
 }
