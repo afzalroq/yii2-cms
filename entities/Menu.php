a8ed29d3e76be3792feb41e32f30db46c3f0ad92
Azam2926 <azam.inoyatov.1998@gmail.com>
2021-01-25 12:41:32 +0500

    fix menu link

--- entities/Menu.php
+++ entities/Menu.php
@@ -24,13 +24,16 @@ use yii\db\ActiveRecord;
  */
 class Menu extends ActiveRecord
 {
+    #region Constants
     const TYPE_EMPTY = 1;
     const TYPE_ACTION = 2;
     const TYPE_LINK = 3;
     const TYPE_OPTION = 4;
     const TYPE_ITEM = 5;
     const TYPE_ENTITY_ITEM = 10;
+    #endregion
 
+    #region Extra Attributes
     public $types;
     public $types_helper;
     public $option_id;
@@ -39,24 +42,92 @@ class Menu extends ActiveRecord
     public $link;
 
     private $CMSModule;
+    #endregion
 
+    #region Overrides
     public function __construct($config = [])
     {
         $this->CMSModule = Yii::$app->getModule('cms');
         parent::__construct($config);
     }
 
-
-    public function getMaxSort()
-    {
-        return ((new ActiveQuery(self::class))
-                ->from(self::tableName())
-                ->max('sort')) + 1;
-    }
-
     public static function tableName()
     {
         return 'cms_menu';
+    }
+
+    public function beforeSave($insert)
+    {
+        if (!parent::beforeSave($insert))
+            return false;
+
+        if (!$this->type_helper) {
+            $this->addError('types_helper', Yii::t('cms', 'Type helper required'));
+            return false;
+        }
+        switch ($this->type) {
+            case self::TYPE_ITEM:
+                $this->type_helper = explode('_', $this->types)[1] . ',' . $this->type_helper;
+                break;
+        }
+        return true;
+    }
+
+    public function afterFind()
+    {
+        $options = '';
+        switch ($this->type) {
+            case self::TYPE_EMPTY:
+                $this->types = 'type_' . $this->type;
+                break;
+            case self::TYPE_ACTION:
+                $this->types = 'action_' . $this->type_helper;
+                break;
+            case self::TYPE_LINK:
+                $this->types = 'type_' . $this->type;
+                $this->link = $this->type_helper;
+                break;
+            case self::TYPE_OPTION:
+                if ($dependOption = Options::findOne($this->type_helper)) {
+                    foreach (Options::findAll(['collection_id' => $dependOption->collection_id]) as $option)
+                        $options .= '<option ' . (($this->type_helper == $option->id) ? 'selected' : '') . ' value=' . $option->id . '>' . $option->name_0 . '</option>';
+
+                }
+                $this->types = 'collection_' . $dependOption->collection->id;
+                $this->types_helper = $options;
+                break;
+            case self::TYPE_ITEM:
+                $option_id = explode(',', $this->type_helper)[0];
+                $item_id = explode(',', $this->type_helper)[1];
+                foreach (OaI::findAll(['option_id' => $option_id]) as $oai)
+                    $options .= '<option ' . (($oai->item_id == $item_id) ? 'selected' : '') . ' value=' . $oai->item_id . '>' . $oai->item->text_1_0 . '</option>';
+
+                $this->types = 'option_' . $option_id;
+                $this->types_helper = $options;
+                $this->option_id = $option_id;
+                $this->type_helper = $item_id;
+                break;
+            case self::TYPE_ENTITY_ITEM:
+                if ($dependItem = Items::findOne($this->type_helper)) {
+                    $entity_id = $dependItem->entity->id;
+                    $this->types = 'entity_' . $entity_id;
+                    foreach (Items::findAll(['entity_id' => $entity_id]) as $item)
+                        $options .= '<option ' . (($item->id == $this->type_helper) ? 'selected' : '') . ' value=' . $item->id . '>' . $item->text_1_0 . '</option>';
+                }
+
+                $this->types_helper = $options;
+                break;
+            default:
+                $this->type_helper = '';
+        }
+        parent::afterFind();
+    }
+
+    public function behaviors()
+    {
+        return [
+            TimestampBehavior::class,
+        ];
     }
 
     public function rules()
@@ -96,6 +167,42 @@ class Menu extends ActiveRecord
             }, 'enableClientValidation' => false],
 
         ];
+    }
+
+    public function attributeLabels()
+    {
+        $language0 = isset(Yii::$app->params['cms']['languages'][0]) ? Yii::$app->params['cms']['languages'][0] : '';
+        $language1 = isset(Yii::$app->params['cms']['languages'][1]) ? Yii::$app->params['cms']['languages'][1] : '';
+        $language2 = isset(Yii::$app->params['cms']['languages'][2]) ? Yii::$app->params['cms']['languages'][2] : '';
+        $language3 = isset(Yii::$app->params['cms']['languages'][3]) ? Yii::$app->params['cms']['languages'][3] : '';
+
+        return [
+            'id' => Yii::t('cms', 'ID'),
+            'parent_id' => Yii::t('cms', 'Parent ID'),
+            'title_0' => Yii::t('cms', 'Title') . '(' . $language0 . ')',
+            'title_1' => Yii::t('cms', 'Title') . '(' . $language1 . ')',
+            'title_2' => Yii::t('cms', 'Title') . '(' . $language2 . ')',
+            'title_3' => Yii::t('cms', 'Title') . '(' . $language3 . ')',
+            'type' => Yii::t('cms', 'Type'),
+            'types' => Yii::t('cms', 'Type'),
+            'type_helper' => Yii::t('cms', 'Type Helper'),
+            'action' => Yii::t('cms', 'Module action'),
+            'link' => Yii::t('cms', 'link'),
+            'page_id' => Yii::t('cms', 'Page'),
+            'articles_category_id' => Yii::t('cms', 'Articles'),
+            'sort' => Yii::t('cms', 'Sort'),
+            'created_at' => Yii::t('cms', 'Created At'),
+            'updated_at' => Yii::t('cms', 'Updated At'),
+        ];
+    }
+    #endregion
+
+    #region Extra Methods
+    public function getMaxSort()
+    {
+        return ((new ActiveQuery(self::class))
+                ->from(self::tableName())
+                ->max('sort')) + 1;
     }
 
     public function actionsList($flip = false)
@@ -158,109 +265,5 @@ class Menu extends ActiveRecord
     {
         return $this->hasOne(self::class, ['id' => 'parent_id']);
     }
-
-    public function attributeLabels()
-    {
-        $language0 = isset(Yii::$app->params['cms']['languages'][0]) ? Yii::$app->params['cms']['languages'][0] : '';
-        $language1 = isset(Yii::$app->params['cms']['languages'][1]) ? Yii::$app->params['cms']['languages'][1] : '';
-        $language2 = isset(Yii::$app->params['cms']['languages'][2]) ? Yii::$app->params['cms']['languages'][2] : '';
-        $language3 = isset(Yii::$app->params['cms']['languages'][3]) ? Yii::$app->params['cms']['languages'][3] : '';
-
-        return [
-            'id' => Yii::t('cms', 'ID'),
-            'parent_id' => Yii::t('cms', 'Parent ID'),
-            'title_0' => Yii::t('cms', 'Title') . '(' . $language0 . ')',
-            'title_1' => Yii::t('cms', 'Title') . '(' . $language1 . ')',
-            'title_2' => Yii::t('cms', 'Title') . '(' . $language2 . ')',
-            'title_3' => Yii::t('cms', 'Title') . '(' . $language3 . ')',
-            'type' => Yii::t('cms', 'Type'),
-            'types' => Yii::t('cms', 'Type'),
-            'type_helper' => Yii::t('cms', 'Type Helper'),
-            'action' => Yii::t('cms', 'Module action'),
-            'link' => Yii::t('cms', 'link'),
-            'page_id' => Yii::t('cms', 'Page'),
-            'articles_category_id' => Yii::t('cms', 'Articles'),
-            'sort' => Yii::t('cms', 'Sort'),
-            'created_at' => Yii::t('cms', 'Created At'),
-            'updated_at' => Yii::t('cms', 'Updated At'),
-        ];
-    }
-
-    public function behaviors()
-    {
-        return [
-            TimestampBehavior::class,
-        ];
-    }
-
-    public function beforeSave($insert)
-    {
-//        if (!$this->type_helper) {
-//            $this->addError('types_helper', Yii::t('cms', 'Type helper required'));
-//            return false;
-//        }
-        if (parent::beforeSave($insert)) {
-            switch ($this->type) {
-                case self::TYPE_LINK:
-                    $this->type_helper = $this->link;
-                    break;
-                case self::TYPE_ITEM:
-                    $this->type_helper = explode('_', $this->types)[1] . ',' . $this->type_helper;
-                    break;
-            }
-            return true;
-        }
-        return false;
-    }
-
-    public function afterFind()
-    {
-        $options = '';
-        switch ($this->type) {
-            case self::TYPE_EMPTY:
-                $this->types = 'type_' . $this->type;
-                break;
-            case self::TYPE_ACTION:
-                $this->types = 'action_' . $this->type_helper;
-                break;
-            case self::TYPE_LINK:
-                $this->types = 'type_' . $this->type;
-                $this->link = $this->type_helper;
-                break;
-            case self::TYPE_OPTION:
-                if ($dependOption = Options::findOne($this->type_helper)) {
-                    foreach (Options::findAll(['collection_id' => $dependOption->collection_id]) as $option)
-                        $options .= '<option ' . (($this->type_helper == $option->id) ? 'selected' : '') . ' value=' . $option->id . '>' . $option->name_0 . '</option>';
-
-                }
-                $this->types = 'collection_' . $dependOption->collection->id;
-                $this->types_helper = $options;
-                break;
-            case self::TYPE_ITEM:
-                $option_id = explode(',', $this->type_helper)[0];
-                $item_id = explode(',', $this->type_helper)[1];
-                foreach (OaI::findAll(['option_id' => $option_id]) as $oai)
-                    $options .= '<option ' . (($oai->item_id == $item_id) ? 'selected' : '') . ' value=' . $oai->item_id . '>' . $oai->item->text_1_0 . '</option>';
-
-                $this->types = 'option_' . $option_id;
-                $this->types_helper = $options;
-                $this->option_id = $option_id;
-                $this->type_helper = $item_id;
-                break;
-            case self::TYPE_ENTITY_ITEM:
-                if ($dependItem = Items::findOne($this->type_helper)) {
-                    $entity_id = $dependItem->entity->id;
-                    $this->types = 'entity_' . $entity_id;
-                    foreach (Items::findAll(['entity_id' => $entity_id]) as $item)
-                        $options .= '<option ' . (($item->id == $this->type_helper) ? 'selected' : '') . ' value=' . $item->id . '>' . $item->text_1_0 . '</option>';
-                }
-
-                $this->types_helper = $options;
-                break;
-            default:
-                $this->type_helper = '';
-        }
-        parent::afterFind();
-    }
-
+    #endregion
 }
