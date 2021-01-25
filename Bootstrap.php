07c069a74bffc0105bfa00481c5309694dbfb512
Abdualiym <a.abdualiym@gmail.com>
2021-01-25 22:41:23 +0500

    fixes

--- Bootstrap.php
+++ Bootstrap.php
@@ -81,3 +81,16 @@ class Bootstrap implements BootstrapInterface
         ]);
     }
 }
+
+
+/**
+ * Dump variables and die.
+ */
+if ( ! function_exists('dd') ) {
+
+    function dd() {
+        call_user_func_array( 'dump' , func_get_args() );
+        die();
+    }
+
+}
