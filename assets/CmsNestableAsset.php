ad4483d027be33f2a1fb02a73a4cc1ad4eeea4ef
Azam2926 <azam.inoyatov.1998@gmail.com>
2021-02-01 19:49:36 +0500

    fix publish assets

--- assets/CmsNestableAsset.php
+++ assets/CmsNestableAsset.php
@@ -2,15 +2,21 @@
 
 namespace afzalroq\cms\assets;
 
-use yii\web\AssetBundle;
-use yii\web\JqueryAsset;
+use kartik\base\AssetBundle;
 
-class CmsNestableAsset extends \kartik\base\AssetBundle
+class CmsNestableAsset extends AssetBundle
 {
-    public function init() {
+    public function init()
+    {
         $this->setSourcePath(__DIR__);
         $this->setupAssets('js', ['js/jquery.nestable']);
         $this->setupAssets('css', ['css/nestable']);
+        $this->publishOptions = [
+            'only' => [
+                'css/*',
+                'js/*'
+            ]
+        ];
         parent::init();
     }
 }
