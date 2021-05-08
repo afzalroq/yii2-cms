d749375e505832a3b14ca28f407cb4cf26808984
Abdualiym <a.abdualiym@gmail.com>
2021-05-08 14:23:23 +0500

    fix migrations

--- migrations/m210507_132100_fixes.php
+++ migrations/m210507_132100_fixes.php
@@ -6,6 +6,54 @@ class m210507_132100_fixes extends Migration
 {
     public function safeUp()
     {
+        //
+        $this->createTable('{{%cms_options_and_menu_types}}', [
+            'id' => $this->primaryKey(),
+            'option_id' => $this->integer()->notNull(),
+            'menu_type_id' => $this->integer()->notNull(),
+            'created_at' => $this->integer()->unsigned()->notNull(),
+            'updated_at' => $this->integer()->unsigned()->notNull(),
+        ]);
+
+        $this->createIndex('index-cms_options_and_menu_types_option_id', 'cms_options_and_menu_types', 'option_id');
+        $this->addForeignKey('fkey-cms_options_and_menu_types_option_id', 'cms_options_and_menu_types', 'option_id', 'cms_options', 'id');
+
+        $this->createIndex('index-cms_options_and_menu_types_menu_type_id', 'cms_options_and_menu_types', 'menu_type_id');
+        $this->addForeignKey('fkey-cms_options_and_menu_types_menu_type_id', 'cms_options_and_menu_types', 'menu_type_id', 'cms_menu_type', 'id');
+
+
+        //
+        $this->addColumn('cms_entities', 'disable_watermark', $this->tinyInteger());
+
+
+        //
+        //cms options
+        $this->addColumn('cms_options', 'created_by', $this->integer());
+        $this->addColumn('cms_options', 'updated_by', $this->integer());
+        //cms items
+        $this->addColumn('cms_items', 'created_by', $this->integer());
+        $this->addColumn('cms_items', 'updated_by', $this->integer());
+        //cms menu
+        $this->addColumn('cms_menu', 'created_by', $this->integer());
+        $this->addColumn('cms_menu', 'updated_by', $this->integer());
+
+
+        //
+        $this->renameColumn('cms_entities','disable_watermark','use_watermark');
+
+
+        //
+        $this->renameColumn('cms_items','date','date_0');
+
+        //adding columns to items
+        $this->addColumn('cms_items','date_1',$this->integer()->unsigned());
+        $this->addColumn('cms_items','date_2',$this->integer()->unsigned());
+        $this->addColumn('cms_items','date_3',$this->integer()->unsigned());
+        $this->addColumn('cms_items','date_4',$this->integer()->unsigned());
+
+
+
+        //
         $this->dropForeignKey('fkey-cms_options_and_items-option_id', 'cms_options_and_items');
         $this->addForeignKey('fkey-cms_options_and_items-option_id', 'cms_options_and_items', 'option_id', 'cms_options', 'id', 'CASCADE', 'CASCADE');
 
