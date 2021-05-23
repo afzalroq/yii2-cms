8d4048748433c609d7eaafc5419600fb85e36cd9
Abdualiym <a.abdualiym@gmail.com>
2021-05-23 11:19:12 +0500

    create and save fix

--- views/items/_form.php
+++ views/items/_form.php
@@ -43,10 +43,10 @@ $hasTranslatableAttrs = 0;
         }
     </style>
     <div class="sticky">
-        <?= Html::submitButton('<i class="fa fa-plus"></i> ' . Yii::t('cms', 'Save and Add new'), ['class' => 'btn btn-primary', 'name' => 'save', 'value' => 'addNew']) ?>
-        <?= Html::submitButton('<i class="fa fa-check"></i> ' . Yii::t('cms', 'Save and Close'), ['class' => 'btn btn-warning', 'name' => 'save', 'value' => 'saveClose']) ?>
+        <?= Html::submitButton('<i class="fa fa-plus"></i> ' . Yii::t('cms', 'Save and Add new'), ['class' => 'btn btn-primary', 'name' => 'save', 'value' => 'add-new']) ?>
+        <?= Html::submitButton('<i class="fa fa-check"></i> ' . Yii::t('cms', 'Save and Close'), ['class' => 'btn btn-warning', 'name' => 'save', 'value' => 'save-close']) ?>
         <?= Html::submitButton('<i class="fa fa-refresh"></i> ' . Yii::t('cms', 'Save'), ['class' => 'btn btn-success', 'name' => 'save', 'value' => 'save']) ?>
-        <?= Html::submitButton('<i class="fa fa-close"></i> ' . Yii::t('cms', 'Close'), ['class' => 'btn btn-danger pull-right', 'name' => 'save', 'value' => 'close', 'style' => 'margin-right: 10px;']) ?>
+        <?= Html::a('<i class="fa fa-close"></i> ' . Yii::t('cms', 'Close'), ['items/index', 'slug' => $entity->slug], ['class' => 'btn btn-danger pull-right', 'name' => 'save', 'value' => 'close', 'style' => 'margin-right: 10px;']) ?>
     </div>
     <?= $form->errorSummary($model) ?>
     <?= $form->field($model, 'entity_id')->textInput(['value' => $entity->id, 'type' => 'hidden'])->label(false) ?>
