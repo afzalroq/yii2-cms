5f5815b182a2d7279d9e04accea03717d5b8ff9b
Abdualiym <a.abdualiym@gmail.com>
2021-06-18 11:34:34 +0500

    fix image

--- components/Image.php
+++ components/Image.php
@@ -31,12 +31,13 @@ class Image
         } else {
             $path->{$operation}($width, $height, $background);
         }
-
         if (isset($obj->entity) && $obj->entity->use_watermark && is_file($module->watermark)) {
             $watermark = GregImage::open($module->watermark)->cropResize(intval($width * 15 / 100), intval($height * 15 / 100));
-            $path->merge($watermark, $width - intval($width * 10 / 100),
-                $height - intval($width * 10 / 100));
+            $path->merge($watermark, $width - intval($width * 10 / 100), $height - intval($width * 10 / 100));
         }
+//        if (isset(Yii::$app->params['sss']) && Yii::$app->params['sss']){
+//            dd($path->guess());
+//        }
 
         return $module->host . str_replace($module->path, '', $path->guess());
     }
