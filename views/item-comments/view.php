4a6478a5075af31c75da6587c1e25a0da31f40da
Sarvar05 <sarvarakbarov0505@gmail.com>
2021-08-16 14:36:22 +0500

    fixed Items Comments View breadcumbs

--- views/item-comments/view.php
+++ views/item-comments/view.php
@@ -9,7 +9,7 @@ use afzalroq\cms\entities\Entities;
 /* @var $entity \afzalroq\cms\entities\Entities */
 
 $this->title = \yii\helpers\StringHelper::truncate($item->text_1_0, 40, '...');
-$this->params['breadcrumbs'][] = ['label' => $entity->{"name_" . Yii::$app->params['l'][Yii::$app->language]}, 'url' => ['/cms/item/index', 'slug' => $entity->slug]];
+$this->params['breadcrumbs'][] = ['label' => $entity->{"name_" . Yii::$app->params['l'][Yii::$app->language]}, 'url' => ['/cms/items/index', 'slug' => $entity->slug]];
 $this->params['breadcrumbs'][] = ['label' => $this->title, 'url' => ['/cms/items/view', 'id' => $item->id, 'slug' => $entity->slug]];
 $this->title = Yii::t('cms', 'Comments #') . $model->id;
 $this->params['breadcrumbs'][] = $this->title;
