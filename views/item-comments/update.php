5cd68bc2005e303c1bd736b72f971aab3b2c4b87
Sarvar05 <sarvarakbarov0505@gmail.com>
2021-08-16 14:33:34 +0500

    fixed Items Comments View breadcumbs

--- views/item-comments/update.php
+++ views/item-comments/update.php
@@ -7,7 +7,7 @@ use yii\helpers\Html;
 
 
 $this->title = \yii\helpers\StringHelper::truncate($item->text_1_0, 40, '...');
-$this->params['breadcrumbs'][] = ['label' => $entity->{"name_" . Yii::$app->params['l'][Yii::$app->language]}, 'url' => ['index', 'slug' => $entity->slug]];
+$this->params['breadcrumbs'][] = ['label' => $entity->{"name_" . Yii::$app->params['l'][Yii::$app->language]}, 'url' => ['cms/item/index', 'slug' => $entity->slug]];
 $this->params['breadcrumbs'][] = ['label' => $this->title, 'url' => ['/cms/items/view', 'id' => $item->id, 'slug' => $entity->slug]];
 $this->title = Yii::t('cms', 'Update');
 $this->params['breadcrumbs'][] = $this->title;
