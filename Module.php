28b5d18c2bfd1ef836dd20e2a1a134f923355adc
Sarvar05 <sarvarakbarov0505@gmail.com>
2021-09-19 12:42:10 +0500

    disable cache for dev mode

--- Module.php
+++ Module.php
@@ -26,7 +26,7 @@ class Module extends \yii\base\Module
     public $path;
     public $host;
     public $cache = 'cache';
-    public $cacheDuration = 3600;
+    public $cacheDuration = YII_ENV_DEV ? 0 : 3600;
     public $fallback;
     public $watermark;
     public $imageOperation = 'cropResize';
