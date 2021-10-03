fa09de23cfe55795e208465b3047b0201a9406c9
Sarvar05 <sarvarakbarov0505@gmail.com>
2021-10-03 10:13:38 +0500

    commen notification widget

--- widgets/comments/views/comment-notifications.php
+++ widgets/comments/views/comment-notifications.php
@@ -19,13 +19,5 @@ if($count > 0): ?>
             </ul>
         </li>
     </ul>
-<?php else: ?>
-    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-        <i class="fa fa-bell-o"></i>
-        <!--<span class="label label-warning">0</span>-->
-    </a>
-    <ul class="dropdown-menu">
-        <li class="header">There are not any new notifications</li>
-    </ul>
 <?php endif?>
 
