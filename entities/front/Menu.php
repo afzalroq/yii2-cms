4f93953360b70fd4ed956d6dead7ea5f05c0bc24
Olimjon <a.abdualiym@gmail.com>
2023-09-02 02:55:50 +0500

    revert menu changes

--- entities/front/Menu.php
+++ entities/front/Menu.php
@@ -3,13 +3,14 @@
 namespace afzalroq\cms\entities\front;
 
 use afzalroq\cms\entities\Entities;
+use slatiusa\nestable\Nestable;
 use afzalroq\cms\widgets\menu\MenuWidget;
 use afzalroq\cms\widgets\menu\OptionWidget;
 use yii\base\BaseObject;
 use yii\caching\TagDependency;
 use Yii;
 
-class Menu
+class Menu extends Nestable
 {
     public static function getMenu($slug, $is_menu = true)
     {
