a830c11066821daede2114045aeaf405d1e4ff45
Olimjon <a.abdualiym@gmail.com>
2023-09-16 00:22:41 +0500

    Update index.php

--- views/options/index.php
+++ views/options/index.php
@@ -10,45 +10,47 @@ use yii\helpers\Url;
 /* @var $dataProvider yii\data\ActiveDataProvider */
 /* @var $collection Collections */
 
-$this->title = $collection->{"name_" . Yii::$app->params['l'][Yii::$app->language]};
+$this->title                   = $collection->{"name_" . Yii::$app->params['l'][Yii::$app->language]};
 $this->params['breadcrumbs'][] = $this->title;
 
-$curLang = Yii::$app->params['l-name'][Yii::$app->language];
+$curLang  = Yii::$app->params['l-name'][Yii::$app->language];
 $firstKey = array_key_first(Yii::$app->params['cms']['languages']);
-
+$name     = 'name_' . ($collection->option_name === Collections::OPTION_NAME_COMMON ? '0' : $firstKey);
 ?>
 <div class="collections-index">
 
-    <p>
-        <?= Html::a(Yii::t('cms', 'Create'), ['create', 'slug' => $collection->slug], ['class' => 'btn btn-success']) ?>
-    </p>
+  <p>
+      <?= Html::a(Yii::t('cms', 'Create'), ['create', 'slug' => $collection->slug], ['class' => 'btn btn-success']) ?>
+  </p>
 
-    <div style="overflow: auto; overflow-y: hidden">
-        <?= SortableGridView::widget([
-            'dataProvider' => $dataProvider,
-            'filterModel' => $searchModel,
-            'sortUrl' => Url::to(['sortItem']),
-            'columns' => [
-                [
-                    'content' => function () {
-                        return "<span class='glyphicon glyphicon-resize-vertical'></span>";
-                    },
-                    'contentOptions' => ['style' => 'cursor:move;', 'class' => 'moveItem'],
-                ],
-                ['class' => 'yii\grid\SerialColumn'],
-                [
-                    'attribute' => 'name_' . $firstKey,
-                    'value' => function ($model) use ($firstKey, $collection) {
-                        return Html::a('<i class="fa fa-edit"></i> ' . $model->{'name_' . $firstKey}, [
-                            '/cms/options/update', 'id' => $model->id, 'slug' => $collection->slug
-                        ]);
-                    },
+  <div style="overflow: auto; overflow-y: hidden">
+      <?= SortableGridView::widget([
+          'dataProvider' => $dataProvider,
+          'filterModel'  => $searchModel,
+          'sortUrl'      => Url::to(['sortItem']),
+          'columns'      => [
+              [
+                  'content'        => function () {
+                      return "<span class='glyphicon glyphicon-resize-vertical'></span>";
+                  },
+                  'contentOptions' => ['style' => 'cursor:move;', 'class' => 'moveItem'],
+              ],
+              ['class' => 'yii\grid\SerialColumn'],
+              [
+                  'attribute' => $name,
+                  'value'     => function ($model) use ($name, $collection) {
+                      return Html::a('<i class="fa fa-edit"></i> ' . $model->$name, [
+                          '/cms/options/update',
+                          'id'   => $model->id,
+                          'slug' => $collection->slug,
+                      ]);
+                  },
 //                    'label' => $entity->text_1_label . ' (' . $curLang . ')',
-                    'format' => 'html'
-                ],
-                'slug',
-                'created_at:datetime',
-            ],
-        ]); ?>
-    </div>
+                  'format'    => 'html',
+              ],
+              'slug',
+              'created_at:datetime',
+          ],
+      ]); ?>
+  </div>
 </div>
