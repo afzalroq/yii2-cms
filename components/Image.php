b821f5de85104f512d8f456a7619ac279281cb2c
Olimjon <a.abdualiym@gmail.com>
2024-04-06 18:12:59 +0500

    Update Image.php

--- components/Image.php
+++ components/Image.php
@@ -10,15 +10,14 @@ class Image
 {
     public static function get($obj, $attr, $width, $height, $operation, $background, $xPos, $yPos, $itemPhotos = null)
     {
-
         $file   = $itemPhotos ? $itemPhotos[$attr] : $obj[$attr];
         $module = Yii::$app->getModule('cms');
-        $file   = $module->path . '/data/' . mb_strtolower(StringHelper::basename($itemPhotos ? $itemPhotos::className() : $obj::className())) . '/' . $obj->id . '/' . $file;
+        $file   = $module->path . 'data/' . mb_strtolower(StringHelper::basename($itemPhotos ? $itemPhotos::className() : $obj::className())) . '/' . $obj->id . '/' . $file;
 
         $path = GregImage::open($file)->setCacheDir($module->path . 'cache')->setFallback($module->fallback);
 
         if (!file_exists($file)) {
-            $path = GregImage::open($module->path . '/fallback.png')->setCacheDir($module->path . 'cache');
+            $path = GregImage::open($module->fallback)->setCacheDir($module->path . 'cache');
         }
 
         if (isset($obj[$attr])) {
@@ -40,8 +39,8 @@ class Image
         }
 
         if (isset($obj->entity) && $obj->entity->use_watermark && is_file($module->watermark)) {
-            $watermark = GregImage::open($module->watermark)->scaleResize(intval($width * 15 / 100),
-                intval($height * 15 / 100));
+            $watermark = GregImage::open($module->watermark)
+                ->scaleResize(intval($width * 15 / 100), intval($height * 15 / 100));
             $path->merge(
                 $watermark,
                 $width - intval($width * $module->watermarkRight / 100),
