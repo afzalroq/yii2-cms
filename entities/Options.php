339474556ec69dba8ccf35a673c58b6186098592
Olimjon <a.abdualiym@gmail.com>
2024-07-19 23:41:15 +0500

    Update Options.php

--- entities/Options.php
+++ entities/Options.php
@@ -261,24 +261,29 @@ class Options extends ActiveRecord
                 $collection = $this->collection;
                 foreach ($attrs as $attr) {
                     if ($collection->isTranslateble($attr) || $collection->isTranslatebleNameOrContent($attr)) {
+                        $attr = str_replace('option_', '', $attr);
                         $attrUz = $this->{$attr . '_' . $uz};
                         $attrOz = $this->{$attr . '_' . $oz};
                         if (empty($attrUz) && !empty($attrOz)) {
-                            $attrUz = TextConverter::convertText($attrOz, true);
+                            $this->{$attr . '_' . $uz} = TextConverter::convertText($attrOz, true);
                         } elseif (empty($attrOz) && !empty($attrUz)) {
-                            $attrOz = TextConverter::convertText($attrUz, false);
+                            $this->{$attr . '_' . $oz} = TextConverter::convertText($attrUz, false);
                         }
-//                        if ($entity->isHaveHtmltags($attr)) {
-//                            if (empty($attrUz) && !empty($attrOz)) {
-//                                $attrUz = TextConverter::convert($attrOz, 'to_latin');
-//                            } elseif (empty($attrOz) && !empty($attrUz)) {
-//                                $attrOz = TextConverter::convert($attrUz, 'to_cyrillic');
+//                        if ($collection->isHaveHtmltags($attr)) {
+//                            $attr = str_replace('option_', '', $attr);
+//                            if (empty($this->{$attr . '_' . $uz}) && !empty($this->{$attr . '_' . $oz})) {
+//                                $attrName                  = $this->{$attr . '_' . $oz};
+//                                $this->{$attr . '_' . $uz} = TextConverter::convert($attrName, 'to_latin');
+//                            } elseif (empty($this->{$attr . '_' . $oz}) && !empty($this->{$attr . '_' . $uz})) {
+//                                $attrName                  = $this->{$attr . '_' . $uz};
+//                                $this->{$attr . '_' . $oz} = TextConverter::convert($attrName, 'to_cyrillic');
 //                            }
 //                        } else {
-//                            if (empty($attrUz) && !empty($attrOz)) {
-//                                $attrUz = TextConverter::to_latin($attrOz);
-//                            } elseif (empty($attrOz) && !empty($attrUz)) {
-//                                $attrOz = TextConverter::to_cyrillic($attrUz);
+//                            $attr = str_replace('option_', '', $attr);
+//                            if (empty($this->{$attr . '_' . $uz}) && !empty($this->{$attr . '_' . $oz})) {
+//                                $this->{$attr . '_' . $uz} = TextConverter::to_latin($this->{$attr . '_' . $oz});
+//                            } elseif (empty($this->{$attr . '_' . $oz}) && !empty($this->{$attr . '_' . $uz})) {
+//                                $this->{$attr . '_' . $oz} = TextConverter::to_cyrillic($this->{$attr . '_' . $uz});
 //                            }
 //                        }
                     }
